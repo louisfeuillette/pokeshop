9459d44d8c1d924563e0a787b518c3c8a34b6fb6
FEUILLETTE <louis.feuillette@capgemini.com>
2024-11-12 13:51:34 +0100

    Revert "feature retreive pokemons from fetchPokemon in store"
    This reverts commit 1631d9ba401d97f00879456209579cd05d3d0683.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { Provider } from "react-redux";
-import store from "./app/store.js";
-
 import App from "./app/App.jsx";
 import "./index.css";
 
@@ -11,8 +8,6 @@ const root = createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </StrictMode>
 );
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,31 +1,5 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-
-import {
-  fetchPokemon,
-  getPokemonList,
-} from "../../features/fetch/pokemonSlice";
-
 const Home = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchPokemon());
-  }, []);
-
-  const pokemons = useSelector(getPokemonList);
-  console.log(pokemons[0]);
-
-  return (
-    <>
-      <p>POKESHOP</p>
-      <ul>
-        {pokemons.map((p, i) => (
-          <li key={i}>{p.name}</li>
-        ))}
-      </ul>
-    </>
-  );
+  return <p>POKESHOP</p>;
 };
 
 export default Home;
