ef526cb178fe043e37964abe65767143643e29c2
FEUILLETTE <louis.feuillette@capgemini.com>
2024-11-13 13:11:53 +0100

    feat test carteSliceReducer

---
+++ src/features/cart/carteSlice.test.js
@@ -0,0 +1,39 @@
+import { describe, it, expect } from "vitest";
+
+import cartReducer from "../../features/cart/carteSlice";
+import { cartItems, deleteItem } from "./carteSlice";
+
+describe("cartSlice", () => {
+  it("should return the initial state", () => {
+    const initialState = { cartItems: [] };
+
+    expect(cartReducer(undefined, { type: undefined })).toEqual(initialState);
+  });
+
+  it("should handle cartItems", () => {
+    const previousState = { cartItems: [] };
+    const newItems = [
+      { id: 1, name: "Pikachu" },
+      { id: 2, name: "Charmander" },
+    ];
+
+    const action = cartItems(newItems);
+    const state = cartReducer(previousState, action);
+
+    expect(state.cartItems).toEqual(newItems);
+  });
+
+  it("should handle deleteItem", () => {
+    const previousState = {
+      cartItems: [
+        { id: 1, name: "Pikachu" },
+        { id: 2, name: "Charmander" },
+      ],
+    };
+
+    const action = deleteItem(1);
+    const state = cartReducer(previousState, action);
+
+    expect(state.cartItems).toEqual([{ id: 2, name: "Charmander" }]);
+  });
+});
