ed1f121247a5f39af95a3e5df784a486b7bf296d
FEUILLETTE <louis.feuillette@capgemini.com>
2024-11-13 11:24:32 +0100

    fix cart page bottom nav hidden

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,25 @@
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  useLocation,
+} from "react-router-dom";
 
 import Home from "../screens/Home";
 import Cart from "../screens/Cart";
 import Header from "../components/Header";
 import Bottom from "../components/Bottom";
 
-const HeaderLayout = () => (
-  <>
-    <Header />
-    <Outlet />
-    <Bottom />
-  </>
-);
+const HeaderLayout = () => {
+  const { pathname } = useLocation();
+  return (
+    <>
+      <Header />
+      <Outlet />
+      {pathname !== "/cart" && <Bottom />}
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
--- src/screens/Cart/index.jsx
+++ src/screens/Cart/index.jsx
@@ -2,7 +2,7 @@ import { Container } from "@mui/material";
 
 const Cart = () => {
   return (
-    <Container maxWidth={false}>
+    <Container maxWidth={false} sx={{ height: "85vh" }}>
       <p>wesh la cité</p>
     </Container>
   );
